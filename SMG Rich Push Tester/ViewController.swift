f1934f3370d5e7c2adc1f5274905865941a58c5a
smg <smg@smgs-macbook-pro.local>
2016-08-16 15:09:51 +0300

    Initial Commit

---
+++ SMG Rich Push Tester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SMG Rich Push Tester
+//
+//  Created by smg on 16/08/16.
+//  Copyright © 2016 smg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
